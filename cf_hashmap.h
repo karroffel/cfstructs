10d804131aa86a5cd420f30b4b01a5e48fea6d67
karroffel <therzog@mail.de>
2018-04-28 00:26:48 +0200

    added hashmap documentation

--- cf_hashmap.h
+++ cf_hashmap.h
@@ -2,6 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+///
+/// This is a single-header library that provides a cache-friendly hash map that
+/// uses open addressing with local probing. C99 is needed to compile this for now
+/// (restrict is love), otherwise this library has no further requirements.
+/// Like, *no* further requirements. At all. Not even libc. I hope this can be of use.
+///
+/// As this is a single-header library, just include this file whenever you need to
+/// access its functionality.
+/// IMPORTANT: in at least one file, put
+///
+///  #define CF_HASHMAP_IMPLEMENTATION
+///  #include "cf_hashmap.h"
+///
+/// before the include, as one compilation unit needs to define the functions exposed
+/// by this header.
+///
 #ifndef CF_HASHMAP_H
 #define CF_HASHMAP_H
 
@@ -15,23 +31,55 @@
 	        + (capacity / 4) + 1 \
 	)
 
+/// Calculates the size in bytes for hashmap with estimated `num_elements` entries.
+/// The calculated size gives enough space to not go over a 70% load factor.
+/// Wikipedia says that 70% are a nice spot before the local probing takes too much time.
+/// https://en.wikipedia.org/wiki/Open_addressing
 #define CF_HASHMAP_DECENT_BUFFER_SIZE(key_type, value_type, num_elements) \
 	_CF_HASHMAP_DECENT_BUFFER_SIZE(sizeof(key_type), sizeof(value_type), (int) (1.5 * num_elements))
 
-
+/// Provides information and behavior about the key type used in the map.
 typedef struct {
+
+	/// Size of a key-value in bytes. You probably should use sizeof() to get it.
 	size_t size;
+
+	/// Function pointer to a function that returns true if the keys are the same.
+	/// NOTE: the pointers are to the memory holding the value, not the value itself.
 	bool (*cmp)(const void * restrict, const void * restrict);
+
+	/// Function pointer to a function that copies a key by reading from one location
+	/// and writing to another location. The pointers point to the memory holding the keys,
+	/// not the value itself.
 	void (*copy)(const void * restrict, void * restrict);
 } cf_hashmap_key_info;
 
+/// Provides information about behavior about the value type used in the map.
 typedef struct {
+
+	/// Size of a value-value in bytes. You probably should use sizeof() to get it.
 	size_t size;
+
+	/// Function pointer to a function that copies a value by reading from one location
+	/// and writing to another location. The pointers contain the address of the memory
+	/// holding the value, not the value itself.
 	void (*copy)(const void * restrict, void * restrict);
 } cf_hashmap_value_info;
 
+/// A hashmap type that uses open addressing with local probing.
+/// The hashmap uses 4 different regions of memory: flags, hashes, keys and values.
+/// Those regions are located next to each other in a buffer in a Struct-of-Arrays
+/// kind of fashion.
+/// The flags store information about whether the entry is filled or was filled in the past.
+/// The hashes are the hashes provided by the user. This hashmap doesn't do any hashing itself.
+/// The keys region is used when collisions occur. In such a case the keys will be compared.
+/// The values region contains the values. This hashmap really just touches it to store values.
 typedef struct {
+
+	/// The number of elements in this hash map.
 	size_t num_elements;
+
+	/// The capacity of how many elements *could* be hold in this map.
 	size_t capacity;
 
 	size_t key_size;
@@ -46,21 +94,35 @@ typedef struct {
 } cf_hashmap;
 
 
+/// This function constructs a new hashmap value. The hashmap uses the information about the
+/// key and value type to figure out where to store information in the buffer.
+/// The buffer is a chunk of memory that will be used as the storage. It should probably be
+/// created by using the `CF_HASHMAP_DECENT_BUFFER_SIZE` macro.
 cf_hashmap cf_hashmap_new(cf_hashmap_key_info key_info,
                           cf_hashmap_value_info value_info,
                           size_t buffer_size,
                           void *buffer);
 
+/// Associate a key with a value. The hash is the hash value of the key. This hashmap doesn't
+/// perform any hashing itself, so the caller has to provide the hash.
+/// For collision resolution, the key itself has to be provided as well.
+/// The value pointer points to a memory location that contains the data to be inserted.
 void cf_hashmap_set(cf_hashmap *map,
                     uint32_t hash,
                     const void * restrict key,
                     const void * restrict value);
 
+/// Lookup a value in the hashmap. The hash is the hash of the key. The key value itself
+/// has to be provided in case a collision occurs.
+/// If an entry is found, the value will be written to the out-parameter `value`.
+/// Returns true if an entry was found, false otherwise.
 bool cf_hashmap_lookup(const cf_hashmap *map,
                        uint32_t hash,
                        const void * restrict key,
                        void * restrict value);
 
+/// Remove an entry from the hashtable by proving the hash and the key value, in case a
+/// collision occurs.
 void cf_hashmap_remove(cf_hashmap *map,
                        uint32_t hash,
                        const void * restrict key);
